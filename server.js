936b6c8216798728daf1023f078f8e812ade7bc4
Megan V <elbowgrrl@gmail.com>
2022-03-08 09:20:34 -0800

    sucessfully queried the api and displayed raw data in browser

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 const express = require("express");
-const axios = require('axios');
+const axios = require("axios");
 
 const app = express();
+
+//get tags from client
+const tags = "tech";
 
 // Root route
 app.get("/", (req, res, next) => {
   // console.log("accessing root")
-  res.send("This is the root")
+  res.send("This is the root");
 });
 
 // Route 1 - ping
 app.get("/api/ping", (req, res, next) => {
-  console.log("ping route accessed")
-  res.status(200).json({ "success": true})
+  console.log("ping route accessed");
+  res.status(200).json({ success: true });
 });
 
 // Route 2 - get posts
 app.get("/api/posts", (req, res, next) => {
-const tags = "tech"
-console.log("post route accessed")
-axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=tech`, { tags: tags })
-.then(data => console.log(data.data.posts))
-//need to send the data to the browser
-.catch(err => res.send(err))
+  console.log("post route accessed");
+  axios
+    .get(`https://api.hatchways.io/assessment/blog/posts`, {
+      params: { tag: tags, sortBy: "", direction: "" },
+    })
+    //sort data. Helper functions?
+    .then((data) => res.send(data.data.posts))
+    //need to send the data to the browser. Why can I log it and not send it to the browser?
+    .catch((err) => res.send(err));
 });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
- });
-
+});
