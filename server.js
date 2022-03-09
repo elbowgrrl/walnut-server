0f540a06be57c32bffe21ac1bd664cf8f91028eb
Megan V <elbowgrrl@gmail.com>
2022-03-09 12:10:04 -0800

    working on making multiple 3rd party api calls

--- server.js
+++ server.js
@@ -3,12 +3,34 @@ const axios = require("axios");
 
 const app = express();
 
-//get tags from client
-const paramsObj = { tag: "tech", sortBy: "reads", direction: "desc" };
+const paramsObj = {
+  tag: "tech",
+  params: { sortBy: "reads", direction: "desc" },
+};
+
+//get tags and sorting values from client
+const tags = ["tech", "history"];
+const sortBy = "reads";
+const direction = "desc";
+
+// this function takes in an array of strings and returns an array of serialized url strings
+const serializedURL = function (tagsArray) {
+  console.log("tagsArray.length", tagsArray.length)
+  if (tagsArray.length !== 0) {
+    const urlArray = [];
+    for (tag of tagsArray) {
+      urlArray.push(
+        `https://api.hatchways.io/assessment/solution/posts?tags=${tag}`
+      );
+    }
+    return urlArray;
+  }
+    return null;
+};
 
 // Root route
 app.get("/", (req, res, next) => {
-  // console.log("accessing root")
+  console.log("Root route accessed")
   res.send("There is nothing here. Try /api/ping or /api/posts");
 });
 
@@ -20,13 +42,35 @@ app.get("/api/ping", (req, res, next) => {
 
 // Route 2 - get posts
 app.get("/api/posts", (req, res, next) => {
-  console.log("post route accessed");
+  //stores an array of valid urls, each with a single tag
+  const validSerializedURLs = serializedURL(tags);
+
+  console.log("post route accessed with " + validSerializedURLs);
+
+//   // if validSerializedURLs is null, use res.send to deliver relevant error message
+//   if (!validSerializedURLs) {
+//     res.status(400).json({ error: "Tags parameter is required" });
+//   }
+  
+//   //to use axios.all, an array of axios.get() requests needs to be used.
+//   //it appears that axios.all is depreciated. Use Promise.all instead: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+ 
+//   axios.all(validSerializedURLs).then(axios.spread({...responses}))
+//   .catch((err) => res.send(err));
+// }
+  
+  // make multiple API calls, one for each tag. All should include properly serialized params
   axios
     .get(`https://api.hatchways.io/assessment/blog/posts`, {
-      params: paramsObj
+      params: paramsObj,
     })
+    // aggregate data from multiple API calls
+    // sort data based on provided values
+    // remove duplicate data
+    // send to browser
+
+    // sends data to browser
     .then((data) => res.send(data.data.posts))
-    //need to send the data to the browser. Why can I log it and not send it to the browser?
     .catch((err) => res.send(err));
 });
 
